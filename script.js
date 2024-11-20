27b3e03a18ae23d563c315f213183d17588bd949
SaltyChocolat <rexrares009@gmail.com>
2024-11-20 18:53:56 +0200

    Draw Added Sound and UI Changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Player{
 }
 
 function Cell(){
-    let currentValue = "--";
+    let currentValue = "";
     getCurrent  = ()=> currentValue;
     changeCurrent = (player)=>{
         currentValue = player.value;
@@ -49,15 +49,17 @@ function UIController(){
         
     }
     displayWinner = (activePlayer) =>{
-        console.log(activePlayer.name);
+        alert(activePlayer.name + " has won the game!!");
     }
-    return{displayBoard,displayWinner};
+    displayDraw = ()=>{
+        alert("It's a tie");
+    }
+    return{displayBoard,displayWinner,displayDraw};
 }
 
 function game(){
     activePlayer = null;
     players = []
-    won = false;
 
     board = gameBoard();
     board.initBoard()
@@ -68,18 +70,26 @@ function game(){
             players.push(new Player(0,"X","player1",0));
             players.push(new Player(1,"O","player2",0));
         }
-        won =false;
-        displayScore();
         board.initBoard();
         activePlayer = players[0];  
+        displayScore();
         controller.displayBoard(board);
     }
 
+    reset = ()=>{
+        players = [];
+        players.push(new Player(0,"X","player1",0));
+        players.push(new Player(1,"O","player2",0));
+    
+        init();  
+    }
     displayScore = ()=>{
         score1 = document.querySelector(".score1");
         score2 = document.querySelector(".score2");
-        score1.textContent = `${players[0].name} score: ${players[0].score}`;
-        score2.textContent = `${players[1].name} score: ${players[1].score}`;
+        turn = document.querySelector(".turn");
+        score1.textContent = ` (${players[0].value}) ${players[0].name} score: ${players[0].score}`;
+        score2.textContent = `(${players[1].value}) ${players[1].name} score: ${players[1].score}`;
+        turn.textContent = `It s ${getActivePlayer().value} turn`;
     }
     getPlayers = ()=>players;
 
@@ -92,6 +102,7 @@ function game(){
         ]
         activePlayer = players[0];
         displayScore();
+        init();
 
     }
     checkWin = () =>{
@@ -99,42 +110,54 @@ function game(){
             if (
                 board.getBoard()[i][0].getCurrent() == board.getBoard()[i][1].getCurrent() &&
                 board.getBoard()[i][1].getCurrent() == board.getBoard()[i][2].getCurrent() &&
-                board.getBoard()[i][0].getCurrent() != "--"
+                board.getBoard()[i][0].getCurrent() != ""
             ) {
+                winSound.play();
                 controller.displayWinner(activePlayer);
                 activePlayer.score+=1;
-                won = true;
                 return true;
             } else if (
                 board.getBoard()[0][i].getCurrent() == board.getBoard()[1][i].getCurrent() &&
                 board.getBoard()[1][i].getCurrent() == board.getBoard()[2][i].getCurrent() &&
-                board.getBoard()[0][i].getCurrent() != "--"
+                board.getBoard()[0][i].getCurrent() != ""
             ) {
+                winSound.play();
                 controller.displayWinner(activePlayer);
                 activePlayer.score+=1;
-                won = true;
                 return true;
             } else if (
                 board.getBoard()[0][0].getCurrent() == board.getBoard()[1][1].getCurrent() &&
                 board.getBoard()[1][1].getCurrent() == board.getBoard()[2][2].getCurrent() &&
-                board.getBoard()[0][0].getCurrent() != "--"
+                board.getBoard()[0][0].getCurrent() != ""
             ) {
+                winSound.play();
                 controller.displayWinner(activePlayer);
                 activePlayer.score+=1;
-                won = true;
                 return true;
             } else if (
                 board.getBoard()[2][0].getCurrent() == board.getBoard()[1][1].getCurrent() &&
                 board.getBoard()[1][1].getCurrent() == board.getBoard()[0][2].getCurrent() &&
-                board.getBoard()[2][0].getCurrent() != "--"
+                board.getBoard()[2][0].getCurrent() != ""
             ) {
+                winSound.play();
                 controller.displayWinner(activePlayer);
                 activePlayer.score+=1;
-                won = true;
                 return true;
             }
             
             
+        }
+        let k=0;
+        for(let i =0;i<3;i++){
+            for(let j=0;j<3;j++){
+                if(board.getBoard()[i][j].getCurrent() != "")
+                    k+=1;
+            }
+        }
+        if(k ==9){
+            winSound.play();
+            controller.displayDraw();
+            return true
         }
         return false;
     }
@@ -147,28 +170,40 @@ function game(){
     }
 
     playGame = (event)=>{
-        if(won == false){
-            if(event.target.classList.contains("box")){
-                controller.displayBoard(board);
-                id = event.target.id;
-                player = getActivePlayer();
-                cell = board.getBoard()[Math.floor(id/3)][id%3]
+        if(event.target.classList.contains("box")){
+            controller.displayBoard(board);
+            id = event.target.id;
+            player = getActivePlayer();
+            cell = board.getBoard()[Math.floor(id/3)][id%3]
+            if(cell.getCurrent()==""){
+                clickSound.play();
                 cell.changeCurrent(player);
                 controller.displayBoard(board);
-
-                checkWin();
-                displayScore();
-                switchPlayer();
+                event.target.classList.remove("O");
+                event.target.classList.remove("X");
+                event.target.classList.add(`${player.value}`);
                 
+
+                setTimeout(() => {
+                    if(checkWin()==false){
+                        switchPlayer();
+                        displayScore();
+                    }
+                    else{
+                        init();
+                    } 
+                }, 5);
+
             }
         }
-
-        
+    
     }
 
-    return{playGame,getActivePlayer,switchPlayer,checkWin,init,getPlayers,updatePlayers,displayScore};
+    return{playGame,getActivePlayer,switchPlayer,checkWin,init,getPlayers,updatePlayers,displayScore,reset};
 }
 
+var clickSound = new Audio("click.mp3");
+var winSound = new Audio("tada.mp3");
 
 const gameInstance = game();
 gameInstance.init();
@@ -179,7 +214,7 @@ document.addEventListener("click",gameInstance.playGame.bind(gameInstance));
 
 enterNames = document.querySelector(".enterNames")
 submitNames = document.querySelector(".submitNames");
-startButton = document.querySelector(".startButton");
+resetButton = document.querySelector(".resetButton");
 
 player1 = document.querySelector("#player1");
 player2 = document.querySelector("#player2");
@@ -199,6 +234,9 @@ submitNames.addEventListener("click",function(event){
     dialog.close()
 })
 
-startButton.addEventListener("click",()=> gameInstance.init());
+resetButton.addEventListener("click",()=>{
+    gameInstance.reset()
+    dialog.close();
+});
 
 
